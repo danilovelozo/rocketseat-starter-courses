1a62cd48882792cdf40947f1d72b847caa190f81
Danilo Velozo <velozo.dan@gmail.com>
2020-04-13 23:07:55 -0300

    Add React Native course

---
+++ curso-reactnative/hunt/src/config/StatusBarConfig.js
@@ -0,0 +1,5 @@
+import { StatusBar } from 'react-native';
+
+StatusBar.setBackgroundColor('#DA552F'); // Android
+StatusBar.setBarStyle('light-content'); // iOS
+
---
+++ curso-reactnative/hunt/src/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import Routes from './routes';
+import './config/StatusBarConfig';
+
+const App = () => <Routes />;
+
+export default App;
